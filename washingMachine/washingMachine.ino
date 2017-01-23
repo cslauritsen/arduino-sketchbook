8de41baaf747193ed9544fbbeef9863383425d4c
Chad S. Lauritsen <csl4jc@gmail.com>
2017-01-23 13:48:01 -0500

    backup

--- washingMachine/washingMachine.ino
+++ washingMachine/washingMachine.ino
@@ -1,9 +1,11 @@
-#include <gitcommit.h>
+#include <MsTimer2.h>
+
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 // NODE CONFIGUATION *******************************************
 #define SKETCH_NAME "Washing Machine"
-#define SKETCH_MAJOR_VER "1"
-#define SKETCH_MINOR_VER GIT_COMMIT_ID
+#define SKETCH_MAJOR_VER "3"
+#define SKETCH_MINOR_VER "14"
 
 #define NODEID 0x03
 #define PARENT_NODEID AUTO
@@ -13,6 +15,9 @@
 #define SOUND_PIN 3 // Arduino Digital I/O pin for button/reed switch
 #define SOUND_CHILD_ID 7
 #define WET_CHILD_ID 8
+
+#define STATUS_LED 8
+#define SOUND_LED 7
 
 // ****************************************************************
 #define CSL_DEBUG 1
@@ -37,24 +42,38 @@ MyMessage msgWet(WET_CHILD_ID, V_TRIPPED);
 boolean soundPresent = false;
 boolean wetPresent = false;
 
+void flash() {
+  static boolean output = HIGH;
+
+  digitalWrite(STATUS_LED, output);
+  output = !output;
+}
+
+long soundReset = 0L;
 void onSound() {
-  soundPresent = digitalRead(SOUND_PIN) == LOW;
+  soundPresent = true;  
 }
 void onWet() {
-  wetPresent = digitalRead(WET_PIN) == LOW;
+  wetPresent = true;
 }
 
 void setup()  
 {
- 
-
-  sensor_node.begin(NULL, NODEID, REPEATER_NODE, PARENT_NODEID);
-
   pinMode(SOUND_PIN, INPUT); 
   pinMode(WET_PIN, INPUT);
+ pinMode(STATUS_LED, OUTPUT);
+ pinMode(SOUND_LED, OUTPUT);
+ 
   // Activate internal pull-ups
   digitalWrite(SOUND_PIN, HIGH);
-  digitalWrite(WET_PIN, HIGH);
+  digitalWrite(WET_PIN, HIGH);  
+  
+ digitalWrite(SOUND_LED, LOW);
+ 
+ MsTimer2::set(300, flash);
+
+  MsTimer2::start();
+  sensor_node.begin(NULL, NODEID, REPEATER_NODE, PARENT_NODEID);
   
   // Send the sketch version information to the gateway and Controller
   sensor_node.sendSketchInfo(SKETCH_NAME, SKETCH_MAJOR_VER "." SKETCH_MINOR_VER);
@@ -62,31 +81,45 @@ void setup()
   // Register binary input sensor to sensor_node (they will be created as child devices)
   // You can use S_DOOR, S_MOTION or S_LIGHT here depending on your usage. 
   // If S_LIGHT is used, remember to update variable type you send in. See "msg" above.
-  sensor_node.present(SOUND_CHILD_ID, S_SOUND, "Washer Sound", true);  
-  sensor_node.present(WET_CHILD_ID, S_WATER_LEAK, "Washer Leak", true);
+  sensor_node.present(SOUND_CHILD_ID, S_MOTION, "Washer Sound", true);  
+  sensor_node.present(WET_CHILD_ID, S_MOTION, "Washer Leak", true);
   
-  attachInterrupt(digitalPinToInterrupt(SOUND_PIN), onSound, CHANGE); 
-  attachInterrupt(digitalPinToInterrupt(WET_PIN), onWet, CHANGE); 
+  attachInterrupt(digitalPinToInterrupt(SOUND_PIN), onSound, FALLING); 
+  attachInterrupt(digitalPinToInterrupt(WET_PIN), onWet, FALLING); 
+  MsTimer2::stop();
 }
 
 boolean lastWet = wetPresent;
 boolean lastSound = soundPresent;
 long elapsed = 0;
+
+long soundResetMillis = 0;
+long wetResetMillis = 0;
 void loop() {
+  
+  elapsed = millis();
   sensor_node.process();
+  
+  digitalWrite(SOUND_LED, soundPresent);
+  digitalWrite(STATUS_LED, wetPresent);
+  
+  if (soundPresent && ABS(elapsed - soundResetMillis) > 15000) {
+     soundPresent = digitalRead(SOUND_PIN) == LOW;
+     soundResetMillis = elapsed;
+  }  
+  
+  if (wetPresent && ABS(elapsed - wetResetMillis) > 5000) {
+     wetPresent = digitalRead(WET_PIN) == LOW;
+     wetResetMillis = elapsed;
+  }  
+  
   if (lastWet != wetPresent) {
     sensor_node.send(msgWet.set(wetPresent));
-    lastWet = wetPresent;
   }
   if (lastSound != soundPresent) {
     sensor_node.send(msgSound.set(soundPresent));
-    lastSound = soundPresent;
-  }
+  }  
   
-  if (elapsed > 10L * 60 * 1000) {
-    elapsed = 0;
-    sensor_node.send(msgSound.set(soundPresent));
-    sensor_node.send(msgWet.set(wetPresent));
-  }
-  elapsed = millis();
+  lastSound = soundPresent;
+  lastWet = wetPresent;
 }
